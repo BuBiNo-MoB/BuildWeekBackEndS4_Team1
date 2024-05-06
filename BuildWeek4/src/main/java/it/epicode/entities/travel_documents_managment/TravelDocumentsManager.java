03b9ee828564c572020e099fa7a944fdfcda6d62
Danilon6 <danilonebulosi@gmail.com>
2024-05-06 18:32:21 +0200

    Getting closer

--- BuildWeek4/src/main/java/it/epicode/entities/travel_documents_managment/TravelDocumentsManager.java
+++ BuildWeek4/src/main/java/it/epicode/entities/travel_documents_managment/TravelDocumentsManager.java
@@ -1,24 +1,53 @@
 package it.epicode.entities.travel_documents_managment;
 
 import it.epicode.entities.BaseEntity;
+import it.epicode.entities.Card;
+import it.epicode.entities.travel_documents.Subscription;
+import it.epicode.entities.travel_documents.Ticket;
+import it.epicode.enums.Frequency;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
 
 public class TravelDocumentsManager extends BaseEntity {
-    private boolean active;
+    private static final Logger log = LoggerFactory.getLogger(TravelDocumentsManager.class);
+    private int issuedTickets;
+    private int issuedSubscription;
 
-    public TravelDocumentsManager() {
-        this.active = true;
+
+    public void issueTickets() {
+        Ticket ticket = new Ticket();
+        issuedTickets++;
+        log.info("Tickets succesfully issued. Actually issued Tickets {} ", this.getIssuedTickets() );
+    }
+    public void issueSubscription(Frequency frequency, Card card) {
+        if (!card.getExpiration_date().isAfter(LocalDate.now())){
+            Subscription subscription = new Subscription(frequency, card);
+            issuedSubscription++;
+            log.info("Subscription succesfully issued. Actually issued Subscription {} ", this.getIssuedSubscription() );
+        }
+            log.warn("Unfortunately your card is expired");
     }
 
-    public boolean isActive() {
-        return active;
+    public int getIssuedTickets() {
+        return issuedTickets;
     }
 
-    public void setActive(boolean active) {
-        this.active = active;
+    public void setIssuedTickets(int issuedTickets) {
+        this.issuedTickets = issuedTickets;
+    }
+
+    public int getIssuedSubscription() {
+        return issuedSubscription;
+    }
+
+    public void setIssuedSubscription(int issuedSubscription) {
+        this.issuedSubscription = issuedSubscription;
     }
 
     @Override
     public String toString() {
-        return "TravelDocumentsManager{" + super.toString() + "active=" + active + '}';
+        return "TravelDocumentsManager{" + super.toString() + '}';
     }
 }
