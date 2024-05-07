c5eab62bbadd8c3ca41c28ced8c4f2f5373031c1
Danilon6 <danilonebulosi@gmail.com>
2024-05-07 18:09:28 +0200

    I'm joking it's working (MAYBE)

--- BuildWeek4/src/main/java/it/epicode/dao/implementation/JpaDao.java
+++ BuildWeek4/src/main/java/it/epicode/dao/implementation/JpaDao.java
@@ -5,9 +5,6 @@ import jakarta.persistence.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
 public abstract class JpaDao<T> implements Dao<T> {
     private static final Logger log = LoggerFactory.getLogger(JpaDao.class);
     // Nome della persistence unit nel file persistence.xml
--- BuildWeek4/src/main/java/it/epicode/dao/implementation/JpaTravelDocumentManagerDao.java
+++ BuildWeek4/src/main/java/it/epicode/dao/implementation/JpaTravelDocumentManagerDao.java
@@ -2,7 +2,6 @@ package it.epicode.dao.implementation;
 
 import it.epicode.dao.interfaces.Dao;
 import it.epicode.dao.interfaces.TravelDocumentManagerDao;
-import it.epicode.entities.travel_documents.TravelDocument;
 import it.epicode.entities.travel_documents_managment.TravelDocumentsManager;
 
 public class JpaTravelDocumentManagerDao extends JpaDao<TravelDocumentsManager> implements Dao<TravelDocumentsManager>, TravelDocumentManagerDao {
